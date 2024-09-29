c25a3b957e12505fcf48baf2d62d357473d6aa85
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-09-29 19:59:54 +0800

    add new track music

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import "./App.css";
 import rule from "./assets/rule.mp3";
 import mirror from "./assets/mirror.mp3";
 import invincible from "./assets/invincible.mp3";
-
+import ruletrack from "./assets/rule.png";
+import mirrortrack from "./assets/mirror.png";
+import invincibletrack from "./assets/invincible.png";
+import newgenesistrack from "./assets/newgenesis.png";
+import useewatrack from "./assets/useewa.png";
+import kurakuratrack from "./assets/kurakura.png";
+import newgenesis from "./assets/newgenesis.mp3";
+import useewa from "./assets/useewa.mp3";
+import kurakura from "./assets/kurakura.mp3";
 function App() {
   return (
     <>
@@ -37,6 +45,7 @@ function App() {
           <div className="track-list">
             <div className="track">
               <div className="track-card">
+                <img src={mirrortrack} alt="mirror soundtrack bg" />
                 <span>Track 1: Mirror</span>
                 <audio controls>
                   <source src={mirror} type="audio/mpeg" />
@@ -45,6 +54,7 @@ function App() {
             </div>
             <div className="track">
               <div className="track-card">
+                <img src={ruletrack} alt="rule soundtrack bg" />
                 <span>Track 2: Rule</span>
                 <audio controls>
                   <source src={rule} type="audio/mpeg" />
@@ -53,9 +63,39 @@ function App() {
             </div>
             <div className="track">
               <div className="track-card">
+                <img src={invincibletrack} alt="invincible soundtrack bg" />
                 <span>Track 3: Invincible</span>
                 <audio controls>
                   <source src={invincible} type="audio/mpeg" />
+                </audio>
+              </div>
+            </div>
+          </div>
+          <div className="track-list">
+            <div className="track">
+              <div className="track-card">
+                <img src={useewatrack} alt="useewa soundtrack bg" />
+                <span>Track 4: Useewa</span>
+                <audio controls>
+                  <source src={useewa} type="audio/mpeg" />
+                </audio>
+              </div>
+            </div>
+            <div className="track">
+              <div className="track-card">
+                <img src={kurakuratrack} alt="kura kura soundtrack bg" />
+                <span>Track 5: Kura Kura</span>
+                <audio controls>
+                  <source src={kurakura} type="audio/mpeg" />
+                </audio>
+              </div>
+            </div>
+            <div className="track">
+              <div className="track-card">
+                <img src={newgenesistrack} alt="new genesis soundtrack bg" />
+                <span>Track 6: New Genesis</span>
+                <audio controls>
+                  <source src={newgenesis} type="audio/mpeg" />
                 </audio>
               </div>
             </div>
