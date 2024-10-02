973d0b28ccb9e3496432913c1324e6819352f82e
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-10-02 17:26:53 +0800

    fix the style layout i'm using now map to store the data

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import rule from "./assets/rule.mp3";
+import mirror from "./assets/mirror.mp3";
+import invincible from "./assets/invincible.mp3";
+import ruletrack from "./assets/rule.png";
+import mirrortrack from "./assets/mirror.png";
+import invincibletrack from "./assets/invincible.png";
+import newgenesistrack from "./assets/newgenesis.png";
+import useewatrack from "./assets/useewa.png";
+import kurakuratrack from "./assets/kurakura.png";
+import newgenesis from "./assets/newgenesis.mp3";
+import useewa from "./assets/useewa.mp3";
+import kurakura from "./assets/kurakura.mp3";
+
+export const data = [
+  {
+    id: 1,
+    name: "Mirror",
+    image: mirrortrack,
+    track: mirror,
+  },
+  {
+    id: 2,
+    name: "Rule",
+    image: ruletrack,
+    track: rule,
+  },
+  {
+    id: 3,
+    name: "Invincible",
+    image: invincibletrack,
+    track: invincible,
+  },
+  {
+    id: 4,
+    name: "Useewa",
+    image: useewatrack,
+    track: useewa,
+  },
+  {
+    id: 5,
+    name: "Kura kura",
+    image: kurakuratrack,
+    track: kurakura,
+  },
+  {
+    id: 6,
+    name: "New Genesis",
+    image: newgenesistrack,
+    track: newgenesis,
+  },
+];
