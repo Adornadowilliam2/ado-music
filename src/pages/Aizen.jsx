98f2cb052354b508e31ff1bc10f423bedbaaf252
Adornadowilliam2 <adornadowilliam@gmail.com>
2024-10-09 13:41:47 +0800

    fix the lyrics

--- src/pages/Aizen.jsx
+++ src/pages/Aizen.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Aizen({ Link, image }) {
   const lyrics = [
-    "I can touch but I know you don't feel a thing",
-    "I can pray but I know you commit a sin",
-    "I can sense now it's all become clear to see",
-    "You're no good, and you mean no good treacherously",
     "I can tell that you've never been true to me",
     "I can smell that you're acting so fearfully",
     "I can hear what you're hoping I want to hear",
     "I can feel the alarm bells are ringing in me",
+    "I can touch but I know you don't feel a thing",
+    "I can pray but I know you commit a sin",
+    "I can sense now it's all become clear to see",
+    "You're no good, and you mean no good treacherously",
   ];
 
   return (
